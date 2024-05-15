06e2065e93a6d90cf52a5bc752d60f6d064b5b7a
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2024-05-15 14:39:00 +0930

    Create plot_index_hopping.R

---
+++ plot_index_hopping.R
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(ggstatsplot)
+library(dplyr)
+library(readr)
+
+# get data from GitHub
+urlfile="https://raw.githubusercontent.com/roberta-davidson/Davidson_etal_2024-Twist/main/index_hopping_stats2.tsv"
+all_data<-read_delim(url(urlfile), delim="\t")
+
+#index hopping 1 round of twist
+ggbetweenstats(subset(all_data, rounds="1"), Type, rate, type="r") #+
+
+#index hopping 2 round of twist
+ggbetweenstats(subset(all_data, rounds="2"), Type, rate, type="r")
+
+all_data$Type[all_data$Type == "HE"] <- "4-library Pools"
+all_data$Type[all_data$Type == "LE"] <- "2-library Pools"
+all_data$Type[all_data$Type == "SG"] <- "All Other Pairs of Libraries (Capture & Shotgun)"
+all_data$Type[all_data$Type == "TW"] <- "All Other Pairs of Libraries (Capture & Shotgun)"
+all_data$Type[all_data$Type == "TW_SG"] <- "All Other Pairs of Libraries (Capture & Shotgun)"
+
+all_data$Round_type <- paste0(all_data$Type," ", "TW", all_data$rounds )
+all_data$Round_type[all_data$Round_type == "All Other Pairs of Libraries (Capture & Shotgun) TW1"] <- "All Other Pairs of Libraries (Capture & Shotgun)"
+all_data$Round_type[all_data$Round_type == "All Other Pairs of Libraries (Capture & Shotgun) TW2"] <- "All Other Pairs of Libraries (Capture & Shotgun)"
+
+png("<path>/index_hopping_1.png",
+    width     = 8,
+    height    = 6,
+    units     = "in",
+    res       = 1200)
+ggbetweenstats(all_data, Type, rate, 
+               xlab = "Index Hopping Group", 
+               ylab = "Single-Index Hopping Rate", 
+               type="r", 
+               pairwise.display = "s", 
+               results.subtitle=FALSE) +
+  theme(axis.title.y.right = element_blank(), 
+        axis.text.y.right = element_blank(), 
+        axis.ticks.y.right = element_blank()) +
+  scale_color_manual(values = c("#019dfe", "#001bcc", "#ff5500"))
+dev.off()
+
+png("<path>/index_hopping_2.png",
+    width     = 12,
+    height    = 6,
+    units     = "in",
+    res       = 1200)
+ggbetweenstats(all_data, Round_type, rate, 
+               xlab = "Index Hopping Group", 
+               ylab = "Single-Index Hopping Rate", 
+               type="r", 
+               pairwise.display = "s",
+               results.subtitle=FALSE) +
+  theme(axis.title.y.right = element_blank(), 
+        axis.text.y.right = element_blank(), 
+        axis.ticks.y.right = element_blank()) +
+  scale_color_manual(values = c("#00b6c1", "#007f92", "#01b277", "#00998b", "#ff5500"))
+dev.off()
